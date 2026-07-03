c4729968d51150873b455169ef8cd60b8c82dfbe
MOHAMAD ISHOMUDDIN ROJIB <277196638+jibsz03@users.noreply.github.com>
2026-07-03 17:54:38 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,20 @@ USER_NAME="${SSH_USER:-ddfathu}"
 USER_PASS="${SSH_PASSWORD:-123456}"
 MAIN_PORT="${PORT:-8080}"
 
+# 1. Mengonfigurasi Server Message
+MESSAGE="${SERVER_MESSAGE:-Selamat datang di Server SSH J1BTNL!}"
+
+echo "[*] Mengonfigurasi Server Message..."
+# Menyimpan pesan untuk Banner (sebelum login)
+echo -e "$MESSAGE\n" > /etc/issue.net
+echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
+
+# Menyimpan pesan untuk MOTD (setelah login)
+echo -e "$MESSAGE\n" > /etc/motd
+# Nonaktifkan default motd Ubuntu agar tidak menimpa pesan kustom
+sed -i 's/PrintMotd no/PrintMotd yes/g' /etc/ssh/sshd_config
+rm -f /etc/update-motd.d/*
+
 echo "[*] Mengonfigurasi User SSH..."
 if ! id "$USER_NAME" &>/dev/null; then
     useradd -m -s /bin/bash "$USER_NAME"
